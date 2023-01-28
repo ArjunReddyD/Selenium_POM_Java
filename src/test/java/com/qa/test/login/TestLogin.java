9f3d8d0db80a8f22525179d3782d6422bbe74693
MallikarjunaReddy Duvoori <arjun@MallikarjunaReddys-MacBook-Pro.local>
2023-01-28 11:12:46 -0500

    First commit

---
+++ src/test/java/com/qa/test/login/TestLogin.java
@@ -0,0 +1,17 @@
+package com.qa.test.login;
+
+import java.io.IOException;
+
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestLogin {
+	
+	
+
+	
+	
+	
+
+}
